af7b151f8be862be0bdb1c6161bacdf40183c05d
Luis Enrique Vazquez Escobar <luis.vazquez@irondev.com.mx>
2024-11-17 17:12:13 -0600

    Project Initialization

---
+++ stori/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stori
+//
+//  Created by Personal on 17/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
